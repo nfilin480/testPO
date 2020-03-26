f59831dc231178ad3fac017dac60e9f753944add
Nikolai Filin <nfilin@cs.petrsu.ru>
2020-03-26 20:07:31 +0300

    test

--- app/equation.c
+++ app/equation.c
@@ -7,8 +7,10 @@ int equation(double a, double b, double c, double* x)
     double d = b * b - 4 * a * c;
 
     if (d > 0) {
+	if (a == 0) {x[0] = -c / b; return 1;}
         x[0] = (-b + sqrt(d)) / (2 * a);
         x[1] = (-b - sqrt(d)) / (2 * a);
+	return 2;
     } else if (d == 0) {
         x[0] = (-b + sqrt(d)) / (2 * a);
         return 1;
