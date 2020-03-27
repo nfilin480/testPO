caf3c922ea79901d0ce3c8065e1ddc11f799c72f
Nikolai Filin <nfilin@cs.petrsu.ru>
2020-03-27 09:08:34 +0300

    fix zero

--- app/equation.c
+++ app/equation.c
@@ -2,21 +2,30 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define EPS = 0.000000000000000001
+
 int equation(double a, double b, double c, double* x)
 {
     double d = b * b - 4 * a * c;
 
-    if (d > 0) {
-	if (a == 0) {x[0] = -c / b; return 1;}
+     if (a == EPS && b != EPS) {
+         x[0] = -c / b;
+         return 1;
+     }
+//     if (b == 0){
+//	x[0] = sqrt()
+
+
+
+
+    if (d > EPS && a != EPS) {
         x[0] = (-b + sqrt(d)) / (2 * a);
         x[1] = (-b - sqrt(d)) / (2 * a);
 	return 2;
-    } else if (d == 0) {
+    } else if (d == EPS && a!= EPS) {
         x[0] = (-b + sqrt(d)) / (2 * a);
         return 1;
     }
-        else {
-        return 0;
-    }
-    return 1;
+
+    if (d < EPS) return 0; else return -1;
 }
