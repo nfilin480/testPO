192109c78ce99d3989206a96750cd02b251e7b5f
Nikolai Filin <nfilin@cs.petrsu.ru>
2020-03-27 10:11:34 +0300

    clear trash

--- app/equation.c
+++ app/equation.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
-
 
 int equation(double a, double b, double c, double* x)
 {
     double d = b * b - 4 * a * c;
 
-     if (a == 0 && b != 0) {
+    if (a == 0 && b != 0) {
          x[0] = -c / b;
          return 1;
      }
-//     if (b == 0){
-//	x[0] = sqrt()
-
-
-
 
     if (d > 0 && a != 0) {
         x[0] = (-b + sqrt(d)) / (2 * a);
