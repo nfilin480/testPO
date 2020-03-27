c3ce2950f686e2f83c4b14e04165679ce624d58b
Nikolai Filin <nfilin@cs.petrsu.ru>
2020-03-27 09:35:40 +0300

    fix zero

--- app/equation.c
+++ app/equation.c
@@ -8,7 +8,7 @@ int equation(double a, double b, double c, double* x)
 {
     double d = b * b - 4 * a * c;
 
-     if (a == 0 && b != EPS) {
+     if (a == 0 && b != 0) {
          x[0] = -c / b;
          return 1;
      }
@@ -22,7 +22,7 @@ int equation(double a, double b, double c, double* x)
         x[0] = (-b + sqrt(d)) / (2 * a);
         x[1] = (-b - sqrt(d)) / (2 * a);
 	return 2;
-    } else if (d == 0 && a != 0) {
+    } else if (d == 0 && a != EPS) {
         x[0] = (-b + sqrt(d)) / (2 * a);
         return 1;
     }
