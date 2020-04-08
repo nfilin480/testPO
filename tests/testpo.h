617d015762af4c82a9b48ea93b85d9bf784e967c
Nikolai Filin <nfilin@cs.petrsu.ru>
2020-04-08 10:42:28 +0300

    tests

--- tests/testpo.h
+++ tests/testpo.h
@@ -16,14 +16,14 @@ TEST(equal, no_roots){
 TEST(equal, one_root){
     double* x;
     x = (double*) malloc(2 * sizeof(double));
-    equation(1, 2, 1, x);
+    EXPECT_EQ(equation(1, 2, 1, x), 1);
     EXPECT_EQ(x[0], -1);
 }
 
 TEST(equal, two_roots){
     double* x;
     x = (double*) malloc(2 * sizeof(double));
-    equation(1, -5, 6, x);
+    EXPECT_EQ(equation(1, -5, 6, x), 2);
     EXPECT_EQ(x[0], 3);
     EXPECT_EQ(x[1], 2);
 }
@@ -31,40 +31,14 @@ TEST(equal, two_roots){
 TEST(equal, negative){
     double* x;
     x = (double*) malloc(2 * sizeof(double));
-    equation(0, 1, 2, x);
+    EXPECT_EQ(equation(0, 1, 2, x), 1);
     EXPECT_EQ(x[0], -2);
 }
 
 TEST(equal, negative2){
     double* x;
     x = (double*) malloc(2 * sizeof(double));
-    int result = equation(0, 0, 0, x);
-    EXPECT_EQ(result, -1);
+    EXPECT_EQ(equation(0, 0, 0, x),-1);
 }
-
-TEST(count, no_roots){
-    double* x;
-    x = (double*) malloc(2 * sizeof(double));
-    EXPECT_EQ(equation(1, 2, 4, x), 0);
-}
-
-TEST(count, one_root){
-    double* x;
-    x = (double*) malloc(2 * sizeof(double));
-    EXPECT_EQ(equation(1, 4, 4, x), 1);
-}
-
-TEST(count, two_roots){
-    double* x;
-    x = (double*) malloc(2 * sizeof(double));
-    EXPECT_EQ(equation(1, 8, 4, x), 2);
-}
-
-
-
-
-
-
-
 
 #endif
